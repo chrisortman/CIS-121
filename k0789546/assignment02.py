792c0be7d00275f3953d26ccd026facc3aab4d4d
Bruno Rangel Huerta <bruno14.rangel@gmail.com>
2016-08-30 12:55:50 +0000

    Week 1 assignment

---
+++ k0789546/assignment02.py
@@ -0,0 +1,4 @@
+print "How many days have I been alive?"   
+print "%s days" %(9 + 31 + 365 * 17 + 31 * 4+ 30 * 3 + 29) 
+print "How old am I?"
+print "I am %s years old" % (round(6488 / 365.25, 3)) 
