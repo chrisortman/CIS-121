cae13aec1ada783fc06f61daf761ba44857ae620
Bryant Conquest <bryant.conquest@gmail.com>
2016-08-30 12:55:00 +0000

    Week 1 Assignment

---
+++ k0765042/assignment01.py
@@ -0,0 +1,15 @@
+"""
+Test project
+prints 'Hello World' 10 times
+Along with what number it has printed
+8/25/16
+"""
+
+###Variables
+a = 634
+b = 1
+
+###Main
+while b < a:
+    print "Hello World",b
+    b = b + 1
