d865c1f009e906fd057df742a64b67c3ba051685
J Robert B <k0765197@student.kirkwood.edu>
2016-08-30 13:24:27 +0000

    Week 1 assignment commit

---
+++ k0765197/assignment01.py
@@ -0,0 +1,3 @@
+# define v0 as Hello World and print
+v0 = "Hello World"
+print v0
---
+++ k0765197/assignment02.py
@@ -0,0 +1,45 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 -7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# Do something with math!
+
+print "The time is 1 : 00 PM"
+
+print "What time will it be in 70 minutes?"
+
+if 70 > 60:
+    h = 2
+print h,":",70%60,"PM"
+
+print "What time will it be in 140 minutes?"
+
+if 140 > 120:
+    h = 3
+print h,":",140%60,"PM"
+
+print "What time will it be in 210 minutes?"
+
+if 210 > 180:
+    h = 4
+print h,":",210%60,"PM"
