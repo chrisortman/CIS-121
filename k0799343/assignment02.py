ab9af5d5bb139468145b02113334ff2c236bb66c
Katie Gardner <kpgardner@hotmail.com>
2016-08-30 14:29:14 +0000

    Week 1 Assignment

--- k0799343/assignment02.py
+++ k0799343/assignment02.py
@@ -1,5 +1,11 @@
-print "MATH", 10*525343424>646436453534+4
+math = 10*525343424+646436453534+4
+print "MATH %d" % math
+
 print "MATH2", 24/2<15
-# sombra upload as of 5:18
-print "###", (6.0-5.7067)/0.0038*3
-print ":)"
+
+# as of 5:18 8/26
+
+print "###" 
+
+sombra= (6.0-5.7067)/0.0038*3
+print ":) %d" % sombra
---
+++ k0799343/lpthw/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Katie Gardner'
+my_age = 16
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
