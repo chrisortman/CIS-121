0e8e164baf39c826e9b9e6a791454a52673084ba
Bruno Rangel Huerta <bruno14.rangel@gmail.com>
2016-09-01 12:22:50 +0000

    Week 1 assignment 2

--- k0789546/assignment02.py
+++ k0789546/assignment02.py
@@ -1,4 +1,6 @@
 print "How many days have I been alive?"   
-print "%s days" %(9 + 31 + 365 * 17 + 31 * 4+ 30 * 3 + 29) 
+days = 9 + 31 + 365 * 17 + 31 * 4+ 30 * 3 + 29
+print "%s days" % (days)
 print "How old am I?"
-print "I am %s years old" % (round(6488 / 365.25, 3)) 
+year = 365.25
+print "I am %s years old" % round(days /year , 3)
