a63f4360aacf3cb78a31d411e5c3abfd91c56dd3
Andrew Murley <andrewmurley99@gmail.com>
2016-09-01 12:26:42 +0000

    Week 1 Assignment

--- k0765065/assignment02.py
+++ k0765065/assignment02.py
@@ -1,2 +1,10 @@
-print "How old am I in hours?"
-print 17 * 365 * 24 + (229 * 24), "hours old."
+question = "How old am I in hours?"
+years = 17
+days_in_a_year = 365
+hours_in_a_day = 24
+days_of_this_year = 230
+hours_of_this_day = 8
+h = "hours old"
+
+print question
+print years * days_in_a_year * hours_in_a_day + days_of_this_year * hours_in_a_day + hours_of_this_day, h
