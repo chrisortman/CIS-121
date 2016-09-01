e2bad711e447261ec54c5290e5157d651eb8ff6d
Bryant Conquest <bryant.conquest@gmail.com>
2016-09-01 12:28:40 +0000

    Added in variables to assignment 2

--- k0765042/assignment02.py
+++ k0765042/assignment02.py
@@ -3,12 +3,17 @@ Assignment 2
 
 8/36/16
 """
+
+###Variables
+a = 8 
+b = 5
+
 ####Main
 print "How to find the area of a triangle"
 print "First multiply the base and height of it", 
-print "5 * 8=", 40
+print "5 * 8=", a * b
 print "Second multiply the answer by .5", 
-print "(5*8)*.5=", 20
+print "(5*8)*.5=", (a*b)/2
 print "Now you have your answer of " 
 print "(5*8)*.5= ", 20
 
