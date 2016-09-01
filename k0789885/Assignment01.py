04e90a58717d618d4809d380383fec1c1735480a
Gage Lake <roxiskowaki@gmail.com>
2016-09-01 12:56:27 +0000

    Unit 1 Assignment 2

--- k0789885/Assignment01.py
+++
@@ -1,4 +0,0 @@
-print "How many months are in a Year if all months were 29 days long?"
-print ""
-print "365 / 29"
-print round( 365.25/29.00 , 2 )
---
+++ k0789885/Assignment02.py
@@ -0,0 +1,7 @@
+Year = 365.25
+ShDays = 29.00
+
+print "How many months are in a Year if all months were 29 days long?"
+print "" #Blank line For seperating
+print "365 / 29 = "
+print round( Year/ShDays , 2 )
