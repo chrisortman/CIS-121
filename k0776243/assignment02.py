870412a95b6b11bac98116e1451de72c9fa3ea63
Jessica Tse <jessica-tse@student.kirkwood.edu>
2016-09-01 12:37:42 +0000

    Assignment 2

--- k0776243/assignment02.py
+++ k0776243/assignment02.py
@@ -1,4 +1,10 @@
 #calculate something :) for example "how many chickens?"
-
-print 5 / 1
-print 5 + 3
+5 = feline
+6 = canine
+print "Marshall has," feline "cats and," canine "dogs"
+print "One of each died.  Marshall is sad."
+print feline "- 1"
+print canine "- 1"
+4 = alive_felines
+5 = alive_canines
+print "Now Marshall has only," alive_felines "cats and" alive_canines "dogs."
