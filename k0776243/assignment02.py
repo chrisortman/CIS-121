0a973eb30d37184dd9ee9ae0f3a9dcccd00ad9f8
Jessica Tse <jessica-tse@student.kirkwood.edu>
2016-09-01 13:40:59 +0000

    Assignment 2

--- k0776243/assignment02.py
+++ k0776243/assignment02.py
@@ -1,10 +1,11 @@
 #calculate something :) for example "how many chickens?"
-5 = feline
-6 = canine
-print "Marshall has," feline "cats and," canine "dogs"
+feline = input("How many cats do you have?")
+canine = input("How many dogs do you have?")
+print "Marshall has", feline, "cats and", canine, "dogs"
 print "One of each died.  Marshall is sad."
-print feline "- 1"
-print canine "- 1"
-4 = alive_felines
-5 = alive_canines
-print "Now Marshall has only," alive_felines "cats and" alive_canines "dogs."
+print "Here is the math:"
+print feline, "- 1"
+print canine, "- 1"
+alive_felines = feline - 1
+alive_canines = canine - 1
+print "Now Marshall has only", alive_felines , "cats and", alive_canines, "dogs."
