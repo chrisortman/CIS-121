da4b9a62fbe622490f3548fb553156912726b396
Parker Jensen <pjensen_17@tipton.k12.ia.us>
2016-09-01 12:26:09 +0000

    assignment_1

---
+++ k0795085/Assignment01.py
@@ -0,0 +1,14 @@
+my_age = 17
+days_in_a_year = 365
+
+
+
+
+
+print "What is the product of 36 times 6?", 36 * 6
+
+print "What is the quotient of 89/10", 89.0/10.0
+
+print "What is the sum of 89/10 and 36 times six", 89.0/10.0 * (36 * 6)
+
+print "How many seconds have I been alive", my_age * days_in_a_year* 24 * 60 * 60 
