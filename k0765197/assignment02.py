7ce3bc22b5ee5253c62776b7b551fa24c94811ef
J Robert B <k0765197@student.kirkwood.edu>
2016-09-02 13:57:03 +0000

    Reorganized directories for LPTHW excercises.

--- k0765197/assignment02.py
+++ k0765197/assignment02.py
@@ -27,22 +27,34 @@ print "Is it less or equal?", 5 <= -2
 print "What time is it?"
 
 t = input('Enter the hour... ')
-k = input('Enter the minute... ')
+k = input('Enter the minute(s)... ')
 
 print "What time will it be in an hour and 10 minutes?"
 
 if (70 + k) > 60:
-    h = t + 1
-print h,":",(70+k)%60,"PM"
+    if (10+k)%60 < 10:
+        h = t + 2
+        print h,":",0,(10+k)%60,"PM","Contingent"
+    else:
+        h= t + 1
+        print h,":",(10+k)%60,"PM"
 
 print "What time will it be in 2 hours and 20 minutes?"
 
-if (140+k) > 120:
-    h = t + 2
-print h,":",(140+k)%60,"PM"
+if (80+k) > 60:
+    if (20+k)%60 < 10:
+        h = t + 3
+        print h,":",0,(20+k)%60,"PM","Contingent"
+    else:
+        h = t + 2
+        print h,":",(20+k)%60,"PM"
 
 print "What time will it be in 3 hours and 30 minutes?"
 
-if (210+k) > 180:
-    h = t + 3
-print h,":",(210+k)%60,"PM"
+if (90+k) > 60:
+    if (30+k)%60 < 10:
+        h = t + 4
+        print h,":",0,(30+k)%60,"PM","Contingent"
+    else:
+        h = t + 3
+        print h,":",(30+k)%60,"PM"
