66ea5c5940fe205d6f1ab69acba14dff2a9a753d
Bruno Rangel Huerta <bruno14.rangel@gmail.com>
2016-09-04 00:09:36 +0000

    argv input added

--- k0789546/assignment02.py
+++ k0789546/assignment02.py
@@ -1,8 +1,18 @@
-years = input("What year were you born:\n")  
-age = 2016 - years
+from sys import argv
+
+print "Set the year you were born as an arguement"
+
+script, birthyear = argv
+#years = input("What year were you born:\n")  
+age = 2016 - int(birthyear)
 days = 365.25
 daysTotal = age * days
-print "You are %r years old, and you have been alive for %r days" % (age, daysTotal)
+
+print "You are %s years old, and you have been alive for %s days" % (age, daysTotal)
+print "The program you are running is called %s" % script
+
+
+
 
 
 
