634a1b62b2907da7194f9679d2a11af59ca9c2dc
Josh nemmers <joshnemmers2@gmail.com>
2016-09-06 13:37:22 +0000

    assignment 3

--- k0764898/assignment02.py
+++ k0764898/assignment02.py
@@ -1,5 +1,14 @@
+x = raw_input("how many books do you have")
+print "how many books do I have:"
+
 my_fictional = 90
 print "I have %d fictional books" % my_fictional
 
 my_nonfictional = 60
 print "I have %d nonfictional books" % my_nonfictional
+y = raw_input("what are your fictional groups")
+print "Now lets break it down fictional into groups:"
+my_sciencefiction = 30
+print "I have %d science fiction books" % my_sciencefiction
+my_fantasy = 60
+print "I have %d fantasy books" % my_fantasy
