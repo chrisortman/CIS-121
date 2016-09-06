b0a798dc0d06865462ecbbc4e8635fa3a16b62a5
Andrew Murley <andrewmurley99@gmail.com>
2016-09-06 13:33:41 +0000

    ex15.py - ex17.py

--- k0765065/assignment02.py
+++ k0765065/assignment02.py
@@ -1,10 +1,10 @@
 statement = "You are"
 days_in_a_year = 365
 hours_in_a_day = 24
-days_of_this_year = 231
 h = "hours old"
 
 y = input("How old in years are you? ")
+m = input("How many days has it been since your last birthday? ")
 t = input("How many hours has it been since midnight? ")
 
-print statement, y * days_in_a_year * hours_in_a_day + days_of_this_year * hours_in_a_day + t, h
+print statement, y * days_in_a_year * hours_in_a_day + m * hours_in_a_day + t, h
