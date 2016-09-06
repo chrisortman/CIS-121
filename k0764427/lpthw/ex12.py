4beb0bb4f7dcb64f27c30fe08bc9cc3d1816b43d
Andrew Rompot <k0764427@student.kirkwood.edu>
2016-09-06 13:45:03 +0000

    Examples 3-17

---
+++ k0764427/lpthw/ex12.py
@@ -0,0 +1,5 @@
+print raw_input("How old are you? ")
+print raw_input("How tall are you")
+print raw_input("How much do you weight")
+
+print "So, you're %r old, %r tall and %r heavy" % (age, height, weight)
