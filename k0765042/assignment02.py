8ce1c13aef8e66521f1f57a364418d618793201e
Bryant Conquest <bryant.conquest@gmail.com>
2016-09-06 13:08:31 +0000

    Assignemnt 2

--- k0765042/assignment02.py
+++ k0765042/assignment02.py
@@ -15,5 +15,5 @@ print "5 * 8=", a * b
 print "Second multiply the answer by .5", 
 print "(5*8)*.5=", (a*b)/2
 print "Now you have your answer of " 
-print "(5*8)*.5= ", 20
+print "(5*8)*.5= ", (a*b)/2
 
