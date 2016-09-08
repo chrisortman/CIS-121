a5373269e08f1f03a527ad6090a3a49c7512990e
Katie Gardner <kpgardner@hotmail.com>
2016-09-08 13:06:18 +0000

    9/8/16

--- k0799343/assignment02.py
+++ k0799343/assignment02.py
@@ -1,30 +1,28 @@
+from sys import argv
+
+script, user_name = argv
+prompt = '> '
+
+print "Hi %s, I'm the %s script." % (user_name, script)
+
 math = 10*525343424+646436453534+4
 print "MATH %d" % math
 
 print "MATH2", 24/2<15
 
 print "###" 
+
 current_number=input("Enter current number:")
-sombra= (23.0-current_number)/0.0038*3
-print ":)\n%d" % sombra,
+
+
+sombra= (100.0-current_number)/0.0038*3
+print "%d" % sombra,
 print "in minutes"
 
 print sombra/60,
 print "in hours"
 
 print sombra/60/24,
-print "in days"
-
-print "until 23%"
-
-sombra2= (100.0-current_number)/0.0038*3
-print "%d" % sombra2,
-print "in minutes"\n
-
-print sombra2/60,
-print "in hours"
-
-print sombra2/60/24,
 print "in days"
 
 print "until 100%"
