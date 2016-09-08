8c0478fda36679e2e5aef198542d075b86e0c7d1
Andrew Rompot <k0764427@student.kirkwood.edu>
2016-09-08 12:47:03 +0000

    Assignment 2 with comments

--- k0764427/Assignment2.py
+++ k0764427/Assignment2.py
@@ -1,16 +1,14 @@
-from sys import argv
-script, x = argv 
-print #How old im I?
+from sys import argv # makes it so you can import text for a variable
+script, x = argv # sets x = to whatever you type in
+print "How old im I?" # prints text
 #pls work
-months = 12.0
-weeks = 4.3
-days = 7.0
-hours = 24.0 
-minutes = 60.0
-seconds = 60.0
+months = 12.0 # sets variable
+weeks = 4.3 # sets variable
+days = 7.0 # sets variable 
+hours = 24.0 # sets variable
+minutes = 60.0 # sets variable
+seconds = 60.0 # sets variable
 
-#x = input("What is your age in years ")
-
-print int(x) * months * weeks * days * hours *minutes *seconds, "seconds old"
+print int(x) * months * weeks * days * hours *minutes *seconds, "seconds old" # multiplys all of the diffrent variables
 
 
