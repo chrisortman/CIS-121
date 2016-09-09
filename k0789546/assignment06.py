ed4fe465d3833e44658fa747cce126542a7e34e4
Bruno Rangel Huerta <bruno14.rangel@gmail.com>
2016-09-09 14:45:36 +0000

    Prancercise function

---
+++ k0789546/assignment06.py
@@ -0,0 +1,9 @@
+from sys import argv
+script, text, surround = argv
+
+def prancercise(text,surround):
+    new = text.replace("_", "")
+    b = surround*10
+    print b,new.upper(),b
+prancercise(text,surround)
+#look up zfill
