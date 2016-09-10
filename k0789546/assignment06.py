5692d71f12d624345170a75b8d9ac6ab8152b6f7
Bruno Rangel Huerta <bruno14.rangel@gmail.com>
2016-09-10 02:32:33 +0000

    Underscores replaced with spaces in function

--- k0789546/assignment06.py
+++ k0789546/assignment06.py
@@ -2,8 +2,7 @@ from sys import argv
 script, text, surround = argv
 
 def prancercise(text,surround):
-    new = text.replace("_", "")
+    new = text.replace("_", " ")
     b = surround*10
     print b,new.upper(),b
 prancercise(text,surround)
-#look up zfill
