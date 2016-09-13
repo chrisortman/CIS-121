b72c5e1d741f19ceb7adba802d4a87cec5127c53
Chris Ortman <chris-ortman@uiowa.edu>
2016-09-12 20:00:54 -0500

    Some examples

---
+++ testa.py
@@ -0,0 +1,29 @@
+from sys import argv
+
+# feline = input("How many cats")
+# dog = input("How many dogs")
+
+script,feline,dog = argv
+
+script, feline, dog = ['testa.py',4,3]
+
+_ = 'testa.py'
+feline = 4
+dog = 3
+#
+
+# print "There are %d animals" % (feline + dog)
+(1, 2)o
+
+
+print "You are", ("%d%d%d%d"), h % (y,m,t)
+
+
+You are 39438012 hours old
+
+
+print "You are %d hours old" % hours_old
+
+
+
+
