34d01f7166218de38472d007e23c33c843d52bcc
Logan Walsh <lwalsh_17@tipton.k12.ia.us>
2016-09-13 12:40:03 +0000

    Assignment 6

--- k0459866/Assignments/assignment06.py
+++ k0459866/Assignments/assignment06.py
@@ -6,7 +6,7 @@ def prancercise(text, surround):
     new = text.replace("_", " ")
     d = surround * 10
     return d, new.upper(), d
-
+#returns ('----------', 'SAY HI TO', '----------') ijnstead of (----------SAY HI TO----------)
 prancercise(text, surround)
 transformed = prancercise("say_hi_to", "-")
 
