a649a44fb4d142b0126d38b615c8d1e0eb8217fe
Andrew Murley <andrewmurley99@gmail.com>
2016-09-16 13:41:32 +0000

    ex32.py-ex39.py

--- k0765065/assignment06.py
+++ k0765065/assignment06.py
@@ -3,8 +3,8 @@ from sys import argv
 script, string, repeat = argv
 
 def prancercise(string, repeat):
-    x = string.replace("_", " ")
-    y = repeat * 10
-    print ("%s%s%s") % (y, x.upper(), y)
+    add_space = string.replace("_", " ")
+    dash = repeat * 10
+    print ("%s%s%s") % (dash, add_space.upper(), dash)
 
 prancercise(string, repeat)
