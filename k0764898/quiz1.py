c59af0b384f66d521256ab88f664f5a09b680f3e
Josh nemmers <joshnemmers2@gmail.com>
2016-09-22 13:17:01 +0000

    quiz 1!

---
+++ k0764898/quiz1.py
@@ -0,0 +1,16 @@
+############################
+#
+# Please complete this quiz
+#
+
+# declare an int variable named total with the value of 0
+
+# declare an array named numbers with all numbers 0 - 20
+
+# write a for loop that processes each number
+# if the number is even add it to total
+# if the number is odd and a multiple of 5 display the number preceded by the word Checkpoint: on a single line
+
+#display the value of total followed by a dash and the word total
+
+
