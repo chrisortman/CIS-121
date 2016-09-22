cb32b475afad639c45b78625bd4a83d1ce8cdfea
Logan Walsh <lwalsh_17@tipton.k12.ia.us>
2016-09-22 13:17:23 +0000

    Quiz 1

--- k0459866/Assignments/assignment06.py
+++ k0459866/Assignments/assignment06.py
@@ -1,3 +1,6 @@
+# (BEFORE) D E F I N E D  A N D  P R I N T E D  P R A N C E R C I S E
+# (AFTER) S I M P L Y  P R I N T S P R A N C E R C I S E  T H R O U G H  P R A N C E R C I S E . P Y
+
 from prancercise import prancercise
 # from sys import argv
 
--- k0459866/Assignments/prancercise.py
+++ k0459866/Assignments/prancercise.py
@@ -1,13 +1,5 @@
-# from sys import argv
-
-# script, text, surround = argv
-
+# D E F I N E S  P R A N C E R C I S E
 def prancercise(text, surround):
     new = text.replace("_", " ")
     d = (("-")*10)
     return d+new.upper()+d
-    
-# prancercise(text, surround)
-# transformed = prancercise("say_hi_to", "-")
-
-# print "%s%s%s" % ((("-")*10), transformed, (("-")*10))
