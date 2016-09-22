fe75ca86f22ead4067d51c94f21b2f050536d3b8
Jessica Tse <jessica-tse@student.kirkwood.edu>
2016-09-22 13:13:41 +0000

    Quiz1

---
+++ k0776243/.~c9_invoke_abN5Au.py
@@ -0,0 +1,26 @@
+############################
+#
+# Please complete this quiz
+#
+
+# declare an int variable named total with the value of 0
+total = 0
+
+# declare an array named numbers with all numbers 0 - 20
+numbers =[0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+# write a for loop that processes each number
+if num
+    print numbers + total 
+
+elif numbers(odd, divisible by 5)
+    if True, print Checkpoint
+    
+# if the number is even add it to total
+# if the number is odd and a multiple of 5 display the number preceded by the word Checkpoint: on a single line
+
+#display the value of total followed by a dash and the word total
+
+print total + "- total"
+
+
