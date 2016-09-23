3a6b151731f089eeb798e4125257fd94c80d4024
Andrew Rompot <k0764427@student.kirkwood.edu>
2016-09-23 13:40:17 +0000

    grrrrr

--- k0764427/quiz1.py
+++ k0764427/quiz1.py
@@ -15,14 +15,17 @@
 
 total = 0
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 , 13, 14, 15, 16, 17, 18, 19, 20]
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+checkpoint = []
+checkpoint.append("Checkpoint")
 
 for i in numbers: 
     if i % 2 == 0:
         value = i
         total = total + value
-    else:
-        if i % 5 == 0:
-            print "Checkpoint %d" % value
+    elif value % 5 == 0:
+            checkpoint.append(value)
+print (checkpoint)
 
 print "%d - total" % total
