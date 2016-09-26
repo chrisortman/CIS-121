a53ccd0da0abe96ea11d1527924354b72cbcbb01
Katie Gardner <kpgardner@hotmail.com>
2016-09-26 13:49:09 +0000

    9/26/16

--- k0799343/rgb_game.py
+++ k0799343/rgb_game.py
@@ -1,6 +1,8 @@
 start = "Choose a door."
 idk = "I don't know what that means."
 ch1 = "Blank"
+ch2 = "Blank"
+
 
 print start
 
@@ -11,47 +13,60 @@ while ch1 != "red" and ch1 != "green" and ch1 != "blue":
     if ch1 == "red":
         print "gun or meat?"
         
+        while ch2 != "gun" and ch2 != "meat":
         #gun or meat
-        ch2 = raw_input("ch2>")
-        
-        if ch2 == "gun":
-            print "You kill the tiger and go through the door."
+            ch2 = raw_input("ch2>")
             
-        elif ch2 == "meat":
-            print """
-            You throw the meat to the tiger and run to the door while it is distracted. 
-            Once there however, you find that the door is locked. Turning back you see 
-            a key on the floor by the door you entered throuh, but the tiger is almost
-            done with the meat and will not be distracted for long. What do you do?
-            """
-            
-            #key or break
-            ch3 = raw_input("ch3>")
-            
-            if "key" in ch3:
-                print "you get the key"
+            if "gun" in ch2:
+                print "You kill the tiger and go through the door."
                 
-            elif "break" in ch3:
-                print "You break through the door"
+            elif "meat" in ch2:
+                print """
+                You throw the meat to the tiger and run to the door while it is distracted. 
+                Once there however, you find that the door is locked. Turning back you see 
+                a key on the floor by the door you entered through, but the tiger is almost
+                done with the meat and will not be distracted for long. What do you do?
+                """
+                
+                #key or break
+                ch3 = raw_input("ch3>")
+                
+                if "key" in ch3:
+                    print "you get the key"
+                    
+                elif "break" in ch3:
+                    print "You break through the door"
+                
+            else:
+                print idk
             
-        else:
-            print idk
-        
-    #how do i make it loop back to ch2?
-    
+       
     elif ch1 == "green":
         print "What do you do?"
         
         ch4 = raw_input("ch4>")
         
-        if "lazers" in ch4:
+        if "lazers" in ch4 or "lazer" in ch4:
             print "you slip and die."
             
         elif "panel" in ch4:
             print "You look at the panel"
             
-        
-        
+            ch5 = raw_input("ch5>")
+                
+            if "num" in ch5:
+                print "Correct"
+                
+                ch5a = raw_input("ch5a>")
+                
+                if "num" in ch5a:
+                    print "Correct"
+                    
+                else:
+                    print "Nope"
+                
+            else:
+                print "Nope"
         
         
         
