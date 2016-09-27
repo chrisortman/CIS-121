74bab7f58dee86d0c27d1671e9a0d23a2310201d
Andrew Rompot <k0764427@student.kirkwood.edu>
2016-09-27 13:15:42 +0000

    I think im done

--- k0764427/quiz1.py
+++ k0764427/quiz1.py
@@ -14,6 +14,8 @@
 #display the value of total followed by a dash and the word total
 
 total = 0
+count = 0
+rule = 0
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
@@ -24,10 +26,14 @@ for i in numbers:
     value = i
     if i % 2 == 0:
         total = total + value
-        if value % 5 == 0:
-            checkpoint.append(value)
     elif value % 5 == 0:
+            count = count + 1
             checkpoint.append(value)
-print (checkpoint)
+    else:
+        pass
+while rule < count:
+    rule = rule + 1
+    print "Checkpoint %d" %checkpoint[rule]
+   # print "WORK"    
 
 print "%d - total" % total
