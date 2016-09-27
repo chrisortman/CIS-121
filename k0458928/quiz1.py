1a70fcbf3b3440bab0648b1d7da5976809bb7969
Zach Ford <zford_17@tipton.k12.ia.us>
2016-09-27 13:36:53 +0000

    quiz1.py

--- k0458928/quiz1.py
+++ k0458928/quiz1.py
@@ -13,13 +13,23 @@
 
 #display the value of total followed by a dash and the word total
 
-x = 0
+total = 0
 
 my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-list1 = my_list[0]
-list2 = my_list[1]
-list3 = my_list[2]
 
+def even(x):
+    return x % 2 ==0
+    
+def odd(x):
+    return x % 5 == 0
+    
 for number in my_list:
     print "This is number %d" % number
+    
+for x in my_list:
+    if even(x):
+        total = x + total
+    elif odd(x):
+        print "Checkpoint: %s" % x
+print "%d - total" % total
 
