20ed126bd462486e2290ad73f4bc8c74e1f287fe
Jessica Tse <jessica-tse@student.kirkwood.edu>
2016-09-27 13:25:48 +0000

    Quiz 1

--- k0776243/quiz1.py
+++ k0776243/quiz1.py
@@ -1,8 +1,3 @@
-############################
-#
-# Please complete this quiz
-#
-
 # declare an int variable named total with the value of 0
 total = 0
 
@@ -10,19 +5,29 @@ total = 0
 numbers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 # write a for loop that processes each number
-def even(numbers):
-    print numbers + total
+def even(num):
+    return num % 2 == 0
 
+def odd(num):
+    return num % 5 == 0
 
+for num in numbers:
     
-    return numbers + total 
+    if even(num):
+        total = num + total
+    elif odd(num):
+        print "%d Checkpoint" % num
 
 
-# if the number is even add it to total
+
+
+print  "%s - total" % total
+
+    # if the number is even add it to total
 # if the number is odd and a multiple of 5 display the number preceded by the word Checkpoint: on a single line
 
 #display the value of total followed by a dash and the word total
 
-print total + "- total"s
+
 
 
