ae304c9c00a2ab0ea44285fca85352f5659ab308
Josh nemmers <joshnemmers2@gmail.com>
2016-09-29 12:24:40 +0000

    quiz 1

--- k0764898/quiz1.py
+++ k0764898/quiz1.py
@@ -4,13 +4,17 @@
 #
 
 # declare an int variable named total with the value of 0
-
+total = 0
 # declare an array named numbers with all numbers 0 - 20
+the_numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 # write a for loop that processes each number
-# if the number is even add it to total
+for num in the_numbers
+print "this is count %d" % num
+# if the number is even add it to total 
+if 
 # if the number is odd and a multiple of 5 display the number preceded by the word Checkpoint: on a single line
 
 #display the value of total followed by a dash and the word total
-
+total = 0-total
 
